28ef5fed107efa14239d4dea8df69482e31ee924
Erik Medeiros <erikmedeiros4@gmail.com>
2021-12-13 23:23:28 -0300

    Add buttons for change the paint color
    Added a color picker, a button for either use or not random colors and
    a button to chose to use only 10% of opacity per brush stroke.

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ function createCells(n, line) {
 
         cell.className = "cell"
         cell.addEventListener("mouseenter", () => {
-            cell.style.backgroundColor = color
+            if(useRandomColors) {
+                const randColor = Math.floor(Math.random()*16777215).toString(16);
+                cell.style.backgroundColor = `#${randColor}`
+            } else {
+                cell.style.backgroundColor = color
+            }
+            
+            if(limitOpacity && (opacity = Number(cell.style.opacity)) < 1) {                
+                cell.style.opacity = opacity + 0.1
+            }
 
-            if (!cellsPainted.includes(cell))
+            if(color === "#ffffff") {
+                if(cellsPainted.includes(cell)) {
+                    const index = cellsPainted.indexOf(cell);
+                    cellsPainted.splice(index, index + 1);
+                }
+            }
+            else if (!cellsPainted.includes(cell))
                 cellsPainted.push(cell)
         })
 
@@ -14,28 +29,28 @@ function createCells(n, line) {
     }
 }
 
-function resizeCanvas(canvas, length) {
+function resizeCanvas(canvas, size) {
     const lines = canvas.childNodes
     const numLines = lines.length
 
-    if (numLines < length) {
-        for(let i = 0; i < length; i++) {
+    if (numLines < size) {
+        for(let i = 0; i < size; i++) {
             if(i < numLines) {
-                createCells(length - numLines, lines[i])
+                createCells(size - numLines, lines[i])
             } else {
                 const line = document.createElement("div")
                 line.className = "line"
 
-                createCells(length, line)
+                createCells(size, line)
 
                 canvas.append(line)
             }
         }
     }
-    else if (numLines > length) {
+    else if (numLines > size) {
         for (let i = numLines - 1; i >= 0; i--) {
-            if (i < length) {
-                for (let j = numLines - 1; j >= length; j--) {
+            if (i < size) {
+                for (let j = numLines - 1; j >= size; j--) {
                     lines[i].removeChild(lines[i].childNodes[j])
                 }
             } else {
@@ -47,7 +62,7 @@ function resizeCanvas(canvas, length) {
 
 const canvas = document.querySelector("#canvas")
 const cellsPainted = []
-let color = "black"
+let color = "#000000"
 
 for (let i = 0; i < 16; i++) {
     const line = document.createElement("div")
@@ -61,13 +76,13 @@ for (let i = 0; i < 16; i++) {
 const btnClear = document.querySelector("#clear")
 btnClear.addEventListener("click", () => {
     for (let i = cellsPainted.length - 1; i >= 0; i--) {
-        cellsPainted[i].style.backgroundColor = "white"
+        cellsPainted[i].removeAttribute("style")
         cellsPainted.pop()
     }
 })
 
 const sizeSlider = document.querySelector("#slider")
-sizeSlider.oninput = () => {
+sizeSlider.addEventListener("input", () => {
     const gridSize = Number(sizeSlider.value)
 
     const sliderLabel = document.querySelector("#slider-label")
@@ -75,5 +90,22 @@ sizeSlider.oninput = () => {
 
     btnClear.click()
     resizeCanvas(canvas, gridSize)
-}
+})
 
+const colorPicker = document.querySelector("#color-picker")
+colorPicker.addEventListener("input", () => {
+    color = colorPicker.value
+})
+
+let useRandomColors = false
+const randomCheckbox = document.querySelector("#random-colors")
+randomCheckbox.addEventListener("input", () => {
+    useRandomColors = randomCheckbox.checked
+})
+
+let limitOpacity = false
+const opacityCheckbox = document.querySelector("#opacity")
+opacityCheckbox.addEventListener("input", () => {
+    limitOpacity = opacityCheckbox.checked
+    btnClear.click()
+})
